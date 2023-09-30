d9390f0890c2283728f4eb5e9069501ae80dc290
maoforero <maforeror@gmail.com>
2023-09-29 23:40:36 -0500

    adding models

---
+++ APINetcore/APINetcore/Models/Product.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace APINetcore.Models
+{
+	public class Product
+	{
+		public int Id { get; init; }
+		public string Name { get; set; }
+		public string Description { get; set; }
+		public double Price { get; set; }
+		public DateTime DateClose { get; init; }
+	}
+}
+
