0da1b24c977fee2328e7c6cd26b09806c1112d83
maoforero <maforeror@gmail.com>
2023-09-30 00:00:07 -0500

    GetAll method

---
+++ APINetcore/APINetcore/Repository/ProductsInMemory.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using APINetcore.Models;
+
+namespace APINetcore.Repository
+{
+	public class ProductsInMemory
+	{
+		public readonly List<Models.Product> products = new()
+		{
+			new Models.Product {Id = 1, Name = "Chocorramo", Description = "Biscuit", Price = 2500.00, DateClose = DateTime.Now},
+			new Models.Product {Id = 2, Name = "Agua", Description = "Drink", Price = 1000.00, DateClose = DateTime.Now.AddHours(9)},
+			new Models.Product {Id = 3, Name = "M&Ms", Description = "Candy", Price = 5200.50, DateClose = DateTime.Now.AddMonths(1)},
+			new Models.Product {Id = 4, Name = "Tostacos", Description = "Chips", Price = 1600.20, DateClose = DateTime.Now},
+			new Models.Product {Id = 5, Name = "Detodito", Description = "Chips", Price = 800.99, DateClose = DateTime.Now.AddMonths(-4)},
+		};
+
+		public IEnumerable<Product> GetAllProducts()
+		{
+			return products;
+		}
+	}
+}
+
