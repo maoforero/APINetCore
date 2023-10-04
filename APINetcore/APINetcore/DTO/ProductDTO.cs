11f2eef7aa32cea5d0264ea26db1020520c6a1cf
maoforero <maforeror@gmail.com>
2023-10-03 20:14:49 -0500

    dataAnnotations

--- APINetcore/APINetcore/DTO/ProductDTO.cs
+++ APINetcore/APINetcore/DTO/ProductDTO.cs
@@ -1,11 +1,22 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
+
 namespace APINetcore.DTO
 {
 	public class ProductDTO
 	{
+        [Required]
         public string Name { get; set; }
+
+        [Required]
         public string Description { get; set; }
+
+        [Required]
+        [Range(1, 1000000,
+            ErrorMessage = "Range of {0} admited: {1} to {2}.")]
         public double Price { get; set; }
+
+        [Required]
         public string SKU { get; set; }
 	}
 }
