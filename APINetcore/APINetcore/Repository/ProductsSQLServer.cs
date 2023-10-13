0f1e2e07179270809965bc9cf90e3836dad7ac89
Mao <maforeror@gmail.com>
2023-10-12 23:56:58 -0500

    implemented interface

--- APINetcore/APINetcore/Repository/ProductsSQLServer.cs
+++ APINetcore/APINetcore/Repository/ProductsSQLServer.cs
@@ -1,6 +1,32 @@
-﻿namespace APINetcore.Repository
+﻿using APINetcore.Models;
+
+namespace APINetcore.Repository
 {
-    public class ProductsSQLServer
+    public class ProductsSQLServer : IProductsInMemory
     {
+        public void AddProduct(Product p)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteProduct(string SKU)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Product> GetAllProducts()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Product GetProduct(string SKU)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateProduct(Product p)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
