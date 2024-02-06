ad7efc11934fb970bac1522b204ac4fc2d68d65e
Mao <maforeror@gmail.com>
2024-02-05 21:21:19 -0500

    config nlog in program

--- APINetcore/APINetcore/Program.cs
+++ APINetcore/APINetcore/Program.cs
@@ -1,6 +1,7 @@
 ﻿using APINetcore.DBManagement;
 using APINetcore.Repository;
 using Microsoft.Extensions.Options;
+using NLog.Extensions.Logging;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -17,6 +18,12 @@ builder.Services.AddControllers(Options =>
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// NLog: Configura el proveedor de registro de NLog para ASP.NET Core
+builder.Logging.ClearProviders();
+builder.Logging.SetMinimumLevel(Microsoft.Extensions.Logging.LogLevel.Trace);
+builder.Host.UseNLog();
+
 
 var app = builder.Build();
 
